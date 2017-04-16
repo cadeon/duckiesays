462fe59ff7b6db7f625fb295a4be91da2560ae26
Michael Peter <mpeter88@gmail.com>
2017-04-15 22:18:39 -0400

    index.html added with hardcoded local url.

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var Session = require('koa-session');
 var Views = require('koa-views');
 var Flash = require('koa-flash');
 var Router = require('koa-router');
-var serve = require('koa-static-folder');
+var send    = require('koa-send');
+var serve = require('koa-static');
 var config = require('./config/config');
 var dbConfig = require('./config/knex');
 
@@ -20,9 +21,12 @@ app.use(Helmet());
 app.use(Body());
 app.use(Session(app));
 app.use(Flash());
-app.use(Views(__dirname + '/app/views', { extension: 'ejs' }));
 
 require('./app/routes')(app);
+
+app.use(function* index() {
+  yield send(this, './public/index.html');
+});
 
 var logger = new winston.Logger({transports : winston.loggers.options.transports});
 logger.info('Server started');
