9975638b827c3ed19b9436fd7596cc8875f9dd44
cadeon <cadeon924@gmail.com>
2025-12-24 15:53:10 -0500

    spicy

--- config/config.js
+++ config/config.js
@@ -48,8 +48,8 @@ Your response should only be the response, no additional instructions or context
 You are the oracle speaking, you don't explain yourself further.
 ${prompt}
 		`,
-		max_tokens: 100, // Default max_tokens
-		temperature: 0.6,
+		max_tokens: 300, // Default max_tokens
+		temperature: 0.65,
 	},
 };
 
