e1a6d3c34daecd3f4a3d4e572ce61016cf5c5352
sera <sera@hermes-agent.local>
2026-07-03 19:36:02 -0400

    Add Ben's page at /ben

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,16 @@
+const Router = require('koa-router');
+const send = require('koa-send');
+const path = require('path');
 const thinksRouter = require('./routes/thinks.server.routes');
 
 module.exports = function (app, upload) {
     app.use(thinksRouter.routes());
     app.use(thinksRouter.allowedMethods());
+
+    // Ben's page
+    const benRouter = new Router();
+    benRouter.get('/ben', async (ctx) => {
+        await send(ctx, 'ben.html', { root: path.join(__dirname, '../public') });
+    });
+    app.use(benRouter.routes());
 };
